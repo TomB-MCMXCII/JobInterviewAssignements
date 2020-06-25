851eb3b966dfc512c8d3817638a400dc3bbd7e95
Tomass <blanks.tomass@yahoo.lv>
2020-06-25 21:01:18 +0300

    added console and request projects

---
+++ Abc & Mintos/backend/RedditApi/RedditApi.Request/RedditHttpClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace RedditApi.Request
+{
+    public class RedditHttpClient
+    {
+        public HttpClient client { get; set; }
+        public RedditHttpClient()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("https://oauth.reddit.com");
+        }
+        
+    }
+}
---
+++ Abc & Mintos/backend/RedditApi/RedditApi.Request/RedditRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Http;
+
+namespace RedditApi.Request
+{
+    public class RedditRequest
+    {
+        private RedditHttpClient client { get; set; }
+        public RedditRequest()
+        {
+            client = new RedditHttpClient();
+        }
+    }
+}
