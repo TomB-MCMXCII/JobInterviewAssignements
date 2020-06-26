523962deee926f49d08cd59c166721c9186286ea
Tomass <blanks.tomass@yahoo.lv>
2020-06-26 12:02:09 +0300

    removed console app

---
+++ Abc & Mintos/backend/RedditApi/RedditApi.Request/RedditRequestService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+
+namespace RedditApi.Request
+{
+    public class RedditService
+    {
+       
+        public async void GetBestThreads()
+        {
+            
+        }
+
+        public async void GetTopFiveComments()
+        {
+
+        }
+    }
+}
